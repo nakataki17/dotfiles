c3f1ae951783ea996a99423b29c5cc78513e2ab6
nakataki <52879307+nakataki17@users.noreply.github.com.>
2026-01-02 16:32:24 +0900

    WSL

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,11 +1,18 @@
 local wezterm = require 'wezterm'
 
-return {
-    keys = {
-        -- Option + 矢印キーで単語単位でのカーソル移動(Mac風)
-        -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-        { key = "LeftArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
-        -- Make Option-Right equivalent to Alt-f; forward-word
-        { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
-    }
+local config = {}
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    -- WSLではUbuntuをデフォルトドメインに設定
+    config.default_domain = 'WSL:Ubuntu'
+end
+
+config.keys = {
+    -- Option + 矢印キーで単語単位でのカーソル移動(Mac風)
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = "LeftArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
 }
+    
+return config
